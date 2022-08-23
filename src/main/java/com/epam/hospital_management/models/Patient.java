916d43fc5f486875833262d2f089e321d05c9f7e
Ricardo Valdivieso <ricardo_valdivieso@epam.com>
2022-08-22 22:39:06 -0500

    adding annotations and corrections on classes

--- src/main/java/com/epam/hospital_management/models/Patient.java
+++ src/main/java/com/epam/hospital_management/models/Patient.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
@@ -32,4 +34,8 @@ public class Patient implements Serializable {
     private String lastName;
 
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "id_doctor")
+    private Staff staff;
 }
--- src/main/java/com/epam/hospital_management/models/Staff.java
+++ src/main/java/com/epam/hospital_management/models/Staff.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Column;
@@ -32,9 +33,11 @@ public class Staff implements Serializable {
     private String lastName;
 
     @ManyToOne
+    @JoinColumn(name = "id_role")
     private Role role;
 
     @ManyToOne
+    @JoinColumn(name = "id_speciality")
     private Speciality speciality;
 
 }
