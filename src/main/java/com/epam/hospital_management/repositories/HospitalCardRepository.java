ce6f1f9f9f95d3e2d6c11d55e2baaddf264343aa
Ricardo Valdivieso <ricardo_valdivieso@epam.com>
2022-08-22 20:55:45 -0500

    crudrepository -> jparepository on HospitalCardRep

--- src/main/java/com/epam/hospital_management/repositories/HospitalCardRepository.java
+++ src/main/java/com/epam/hospital_management/repositories/HospitalCardRepository.java
@@ -1,9 +1,9 @@
 package com.epam.hospital_management.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.epam.hospital_management.models.HospitalCard;
 
-public interface HospitalCardRepository extends CrudRepository<HospitalCard, Long> {
+public interface HospitalCardRepository extends JpaRepository<HospitalCard, Long> {
 
 }
