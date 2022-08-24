544c7ae3e762d6976e1ecdc844b50f2dc14cf20c
Ricardo Valdivieso <ricardo_valdivieso@epam.com>
2022-08-23 19:28:35 -0500

    add findAllSorted method using url parameters

--- src/main/java/com/epam/hospital_management/controllers/PatientController.java
+++ src/main/java/com/epam/hospital_management/controllers/PatientController.java
@@ -1,13 +1,16 @@
 package com.epam.hospital_management.controllers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.epam.hospital_management.models.Patient;
@@ -25,9 +28,28 @@ public class PatientController {
         return patientService.findAll();
     }
 
-    @GetMapping("/{field}")
-    private ArrayList<Patient> findAllSorted(@PathVariable String field) {
-        return patientService.findAllSorted(field);
+    @GetMapping(params = "sortBy")
+    private List<Patient> findAllSorted(@RequestParam(defaultValue = "name") String sort) {
+
+        if (sort.equals("name")) {
+            return patientService.findAll()
+                    .stream()
+                    .sorted(Comparator.comparing(Patient::getName))
+                    .collect(Collectors.toList());
+        } else if (sort.equals("lastName")) {
+            return patientService.findAll()
+                    .stream()
+                    .sorted(Comparator.comparing(Patient::getLastName))
+                    .collect(Collectors.toList());
+        } else if (sort.equals("birthDate")) {
+            return patientService.findAll()
+                    .stream()
+                    .sorted(Comparator.comparing(Patient::getBirthDate))
+                    .collect(Collectors.toList());
+        } else {
+            return patientService.findAll();
+        }
+
     }
 
     @PostMapping()
