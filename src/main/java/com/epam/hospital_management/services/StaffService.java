36fc2213de50d72a819416712a178e00acbeb18e
Ricardo Valdivieso <ricardo_valdivieso@epam.com>
2022-08-25 16:44:20 -0500

    cleaning code lines no longer used

--- src/main/java/com/epam/hospital_management/services/StaffService.java
+++ src/main/java/com/epam/hospital_management/services/StaffService.java
@@ -2,10 +2,8 @@ package com.epam.hospital_management.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
