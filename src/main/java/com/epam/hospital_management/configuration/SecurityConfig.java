0c3a10e7eb0d5e6cb593d6569e7bc776a3fd7f4d
Ricardo Valdivieso <ricardo_valdivieso@epam.com>
2022-09-08 22:23:16 -0500

    security branch

---
+++ src/main/java/com/epam/hospital_management/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.epam.hospital_management.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain configfFilterChain(final HttpSecurity https) throws Exception {
+        return https.csrf().and().cors().disable()
+                .authorizeRequests()
+                .antMatchers("/staff").hasRole("ADMIN")
+                .and()
+                .formLogin().defaultSuccessUrl("/staff", true)
+                .and().build();
+    }
+
+}
